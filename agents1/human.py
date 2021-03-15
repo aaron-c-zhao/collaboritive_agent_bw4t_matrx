606ba1899d373b64d1c1b6de7628f155c4d950f4
Pepijn de Kruijff <p.g.m.dekruijff@student.tudelft.nl>
2021-03-16 00:19:33 +0100

    extend human agent

--- agents1/human.py
+++ agents1/human.py
@@ -8,6 +8,8 @@ from matrx.agents.agent_utils.state import State # type: ignore
 from bw4t.BW4TBrain import BW4TBrain
 from matrx.agents import HumanAgentBrain # type: ignore
 
+from matrx.messages import Message
+
 class Human(HumanAgentBrain):
     '''
     Human that can also handle slowdown. Currently not really implemented,
@@ -15,4 +17,17 @@ class Human(HumanAgentBrain):
     '''
     def __init__(self, slowdown:int):
         super().__init__()
-    
+
+
+    def filter_observations(self, state): 
+        blocks = state[{'is_collectable': True}]
+        self.send_message(Message(content=f"testtests  " + str(blocks),
+                                      from_id=self.agent_id,
+                                      to_id=self.agent_id))
+        return super().filter_observations(state)
+
+    def decide_on_bw4t_action(self, state:State):
+        print("test")
+        return super().decide_on_bw4t_action(state)
+
+
